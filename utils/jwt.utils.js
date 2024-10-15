b06fb9cbdbe11fb18bd23ecf254d3466c3a6f158
Mohamed Mostafa <mostafaabogoda8@gmail.com>
2024-10-15 21:17:11 +0300

    c

--- utils/jwt.utils.js
+++ utils/jwt.utils.js
@@ -5,7 +5,7 @@ const generateToken = (user) => {
     return jwt.sign(
         { email: user.email, id: user._id }, 
         process.env.ACCESS_TOKEN_SECRET_KEY, 
-        { expiresIn: '10m' }
+        { expiresIn: '1d' }
     );
 };
 
